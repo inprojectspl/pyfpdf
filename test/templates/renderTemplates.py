00b7d53b2340d6753964df0242ab577ff40c0a99
Iván López Flores <loboestepario91@gmail.com>
2019-08-21 14:26:18 -0500

    Adding test to assert for yaml parse templates

--- test/templates/renderTemplates.py
+++ test/templates/renderTemplates.py
@@ -18,7 +18,7 @@ from test.utilities import relative_path_to, \
                            calculate_hash_of_file
 
 
-class RenderTemplateCSV(unittest.TestCase):
+class RenderTemplate(unittest.TestCase):
 
     def test_RenderCSV(self):
         f = Template(paperformat="A4",
@@ -27,19 +27,36 @@ class RenderTemplateCSV(unittest.TestCase):
         f.parse_csv("testTemplateFile.csv", delimiter=";")
         f.add_page("testTemplateFile.csv")
 
-        #f.render("testTemplateFile.pdf")
         pdf = f.getFPDF()
         set_doc_date_0(pdf)
 
         outfile = relative_path_to("testTemplateFile.pdf")
         pdf.output(outfile, 'F')
-        # print(calculate_hash_of_file(outfile))
 
         test_hash = calculate_hash_of_file(outfile)
-        # ordered the images for reproduceability
+        # Assert versus a well know hash
         self.assertEqual(test_hash, "8d4e2060e5d8264d03ebca707a2ed1ca")
 
         os.unlink(outfile)
+
+    def test_RenderYML(self):
+        f = Template(paperformat="A4",
+                     title="testCSV")
+
+        f.parse_YML("testTemplateFile.yml")
+        f.add_page("testTemplateFile.yml")
+
+        pdf = f.getFPDF()
+        set_doc_date_0(pdf)
+
+        outfile = relative_path_to("testTemplateFile.pdf")
+        pdf.output(outfile, 'F')
+
+        test_hash = calculate_hash_of_file(outfile)
+        # Must reproduce the same file than test_RenderCSV
+        self.assertEqual(test_hash, "8d4e2060e5d8264d03ebca707a2ed1ca")
+
+        #os.unlink(outfile)
 
 
 if __name__ == '__main__':
